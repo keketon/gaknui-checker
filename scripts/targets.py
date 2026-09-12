e4adcd61f9faff69af11cf07b3ae973e98a723b5
keketon <mas08rin10yabko@gmail.com>
2026-09-12 09:59:47 +0900

    Add more nuis

--- scripts/targets.py
+++ scripts/targets.py
@@ -1,5 +1,16 @@
 # TODO: Update query to queries so we can use several queries for a single label (e.g., ["ことね ぬい", "ことね ぬいぐるみ"] "kotone" label)
 TARGETS = [
-    {"category_name": "kotone", "query": "ことね ぬい"},
-    {"category_name": "saki", "query": "咲季 ぬい"}
+    {"category_name": "saki", "query": "咲季 ぬい 学マス"},
+    {"category_name": "temari", "query": "手毬 ぬい 学マス"},
+    {"category_name": "kotone", "query": "ことね ぬい 学マス"},
+    {"category_name": "mao", "query": "麻央 ぬい 学マス"},
+    {"category_name": "lilja", "query": "リーリヤ ぬい 学マス"},
+    {"category_name": "china", "query": "千奈 ぬい 学マス"},
+    {"category_name": "sumika", "query": "清夏 ぬい 学マス"},
+    {"category_name": "hiro", "query": "篠澤広 ぬい 学マス"},
+    {"category_name": "rinami", "query": "莉波 ぬい 学マス"},
+    {"category_name": "ume", "query": "佑芽 ぬい 学マス"},
+    {"category_name": "misuzu", "query": "美鈴 ぬい 学マス"},
+    {"category_name": "sena", "query": "星南 ぬい 学マス"},
+    {"category_name": "tsubame", "query": "燕 ぬい 学マス"}
 ]
